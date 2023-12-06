99007712def1059ea095b1e3adbff5ee33eae300
Kevin Le Faucheur <lefaucheur.ke@gmail.com>
2023-12-06 20:50:58 +0100

    Adding Pickup Widget on Weapon Base

--- Source/Uchronia/Private/Actor/Weapon/Weapon.cpp
+++ Source/Uchronia/Private/Actor/Weapon/Weapon.cpp
@@ -1,7 +1,10 @@
 // Retropsis @ 2023-2024
 
 #include "Actor/Weapon/Weapon.h"
+
+#include "Character/PlayerCharacter.h"
 #include "Components/SphereComponent.h"
+#include "Components/WidgetComponent.h"
 
 /*
  * This should be the base weapon and not used as BP,
@@ -24,6 +27,9 @@ AWeapon::AWeapon()
 	OverlapSphere->SetupAttachment(GetRootComponent());
 	OverlapSphere->SetCollisionResponseToAllChannels(ECR_Ignore);
 	OverlapSphere->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+
+	PickupWidget = CreateDefaultSubobject<UWidgetComponent>(TEXT("PickupWidget"));
+	PickupWidget->SetupAttachment(GetRootComponent());
 }
 
 void AWeapon::BeginPlay()
@@ -34,12 +40,25 @@ void AWeapon::BeginPlay()
 	{
 		OverlapSphere->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
 		OverlapSphere->SetCollisionResponseToChannel(ECC_Pawn, ECR_Overlap);
+		OverlapSphere->OnComponentBeginOverlap.AddDynamic(this, &AWeapon::OnSphereOverlap);
 	}
+	
+	if (PickupWidget) PickupWidget->SetVisibility(false);
 }
 
 void AWeapon::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+}
+
+void AWeapon::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	APlayerCharacter* PlayerCharacter = Cast<APlayerCharacter>(OtherActor);
+	if (PlayerCharacter && PickupWidget)
+	{
+		PickupWidget->SetVisibility(true);
+	}
 }
 
--- Source/Uchronia/Public/Actor/Weapon/Weapon.h
+++ Source/Uchronia/Public/Actor/Weapon/Weapon.h
@@ -6,6 +6,8 @@
 #include "GameFramework/Actor.h"
 #include "Weapon.generated.h"
 
+class UWidgetComponent;
+
 UENUM(BlueprintType)
 enum class EWeaponState : uint8
 {
@@ -30,7 +32,19 @@ public:
 protected:
 	virtual void BeginPlay() override;
 
+	UFUNCTION()
+	virtual  void OnSphereOverlap(
+		UPrimitiveComponent* OverlappedComponent,
+		AActor* OtherActor,
+		UPrimitiveComponent* OtherComp,
+		int32 OtherBodyIndex,
+		bool bFromSweep,
+		const FHitResult & SweepResult);
+
 private:
+	UPROPERTY(VisibleAnywhere, Category="Weapon Properties")
+	EWeaponState WeaponState;
+	
 	UPROPERTY(VisibleAnywhere, Category="Weapon Properties")
 	TObjectPtr<USkeletalMeshComponent> WeaponMesh;
 
@@ -38,5 +52,5 @@ private:
 	TObjectPtr<USphereComponent> OverlapSphere;
 
 	UPROPERTY(VisibleAnywhere, Category="Weapon Properties")
-	EWeaponState WeaponState;
+	TObjectPtr<UWidgetComponent> PickupWidget;
 };
