94449e8142adbb98585f7620ea7eba9894252bc0
Kevin Le Faucheur <lefaucheur.ke@gmail.com>
2023-12-18 22:28:16 +0100

    CharacterClassInfo

---
+++ Source/Uchronia/Public/AbilitySystem/Data/CharacterClassInfo.h
@@ -0,0 +1,57 @@
+// Retropsis @ 2023-2024
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "CharacterClassInfo.generated.h"
+
+class UGameplayEffect;
+
+UENUM(BlueprintType)
+enum class ECharacterClass : uint8
+{
+	Survivor,
+	Mariner,
+	LoneHunter,
+	PackHunter,
+	Worker,
+	Soldier,
+	Vulture,
+	Critter,
+	Elite,
+	Leviathan,
+};
+
+USTRUCT(BlueprintType)
+struct FCharacterClassDefaultInfo
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditDefaultsOnly, Category="Class Defaults")
+	TSubclassOf<UGameplayEffect> PrimaryAttributes;
+
+	
+};
+
+/**
+ * 
+ */
+UCLASS()
+class UCHRONIA_API UCharacterClassInfo : public UDataAsset
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditDefaultsOnly, Category="Class Defaults")
+	TMap<ECharacterClass, FCharacterClassDefaultInfo> CharacterClassDefaultInformation;
+
+	UPROPERTY(EditDefaultsOnly, Category="Common Class Defaults")
+	TSubclassOf<UGameplayEffect> SecondaryAttributes;
+	
+	UPROPERTY(EditDefaultsOnly, Category="Common Class Defaults")
+	TSubclassOf<UGameplayEffect> VitalAttributes;
+
+	FCharacterClassDefaultInfo GetClassDefaultInfo(ECharacterClass CharacterClass);
+};
