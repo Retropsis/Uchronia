70fa176f596597da560c477ccff366e159d8f5e3
Kevin Le Faucheur <lefaucheur.ke@gmail.com>
2023-12-27 16:11:25 +0100

    Melee Weapon Setup, Before reparenting range weapons

---
+++ Source/Uchronia/Public/Actor/Weapon/MeleeWeapon.h
@@ -0,0 +1,17 @@
+// Retropsis @ 2023-2024
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Actor/Weapon/Weapon.h"
+#include "MeleeWeapon.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UCHRONIA_API AMeleeWeapon : public AWeapon
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/Uchronia/Public/Actor/Weapon/RangeWeapon.h
@@ -0,0 +1,17 @@
+// Retropsis @ 2023-2024
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Actor/Weapon/Weapon.h"
+#include "RangeWeapon.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UCHRONIA_API ARangeWeapon : public AWeapon
+{
+	GENERATED_BODY()
+	
+};
