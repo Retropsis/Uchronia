933dc91fca09e7a76c642db2563bda0644f00175
Kevin Le Faucheur <lefaucheur.ke@gmail.com>
2024-01-03 15:00:10 +0100

    Pickup Class Improved

--- Source/Uchronia/Private/Item/Pickup.cpp
+++ Source/Uchronia/Private/Item/Pickup.cpp
@@ -3,45 +3,150 @@
 
 #include "Item/Pickup.h"
 
+#include "ActorComponents/Inventory/InventoryComponent.h"
+#include "ActorComponents/Inventory/ItemBase.h"
 #include "Components/SphereComponent.h"
 #include "Kismet/GameplayStatics.h"
 
 APickup::APickup()
 {
-	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bCanEverTick = false;
 	bReplicates = true;
 
 	RootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("Root"));
 	
+	PickupMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("PickupMesh"));
+	PickupMesh->SetupAttachment(RootComponent);
+	PickupMesh->SetSimulatePhysics(true);
+	PickupMesh->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
+
+	// TODO: Will have to remove this after pickup functionality is done by tracing instead
 	OverlapSphere = CreateDefaultSubobject<USphereComponent>(TEXT("OverlapSphere"));
-	OverlapSphere->SetupAttachment(RootComponent);
+	OverlapSphere->SetupAttachment(PickupMesh);
 	OverlapSphere->SetSphereRadius(OverlapRadius);
 	OverlapSphere->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
 	OverlapSphere->SetCollisionResponseToAllChannels(ECR_Ignore);
 	OverlapSphere->SetCollisionResponseToChannel(ECC_Pawn, ECR_Overlap);
 	// OverlapSphere->AddLocalOffset(FVector(0.f, 0.f, 80.f)); 
+}
 
-	PickupMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("PickupMesh"));
-	PickupMesh->SetupAttachment(OverlapSphere);
-	PickupMesh->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+void APickup::PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent)
+{
+	Super::PostEditChangeProperty(PropertyChangedEvent);
+
+	const FName ChangedPropertyName = PropertyChangedEvent.Property ? PropertyChangedEvent.Property->GetFName() : NAME_None;
+	if(ChangedPropertyName == GET_MEMBER_NAME_CHECKED(APickup, DesiredItemID))
+	{
+		if(ItemDataTable)
+		{
+			if(const FItemData* ItemData = ItemDataTable->FindRow<FItemData>(DesiredItemID, DesiredItemID.ToString()))
+			{
+				PickupMesh->SetStaticMesh(ItemData->AssetData.Mesh);
+			}
+		}
+	}
 }
 
 void APickup::BeginPlay()
 {
 	Super::BeginPlay();
-	if(HasAuthority()) OverlapSphere->OnComponentBeginOverlap.AddDynamic(this, &APickup::OnBeginOverlap);
-}
-
-void APickup::Tick(float DeltaTime)
-{
-	Super::Tick(DeltaTime);
-
+	if(HasAuthority())
+	{
+		OverlapSphere->OnComponentBeginOverlap.AddDynamic(this, &APickup::OnBeginOverlap);
+		InitializePickup(UItemBase::StaticClass(), ItemQuantity);
+	}
 }
 
 void APickup::OnBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
                              UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
 	
+}
+
+/*
+ * Interaction
+ */
+void APickup::BeginFocus()
+{
+	if(PickupMesh)
+	{
+		GEngine->AddOnScreenDebugMessage(1, 3.f, FColor::Red, FString::Printf(TEXT("BeginFocus")));
+		// Mesh->SetRenderCustomDepth(true);
+	}
+}
+
+void APickup::EndFocus()
+{
+	if(PickupMesh)
+	{
+		GEngine->AddOnScreenDebugMessage(2, 3.f, FColor::Red, FString::Printf(TEXT("EndFocus")));
+		// Mesh->SetRenderCustomDepth(false);
+	}
+}
+
+//~ Server Only
+void APickup::InitializePickup(const TSubclassOf<UItemBase> BaseClass, const int32 InQuantity)
+{
+	if(IsValid(ItemDataTable) && !DesiredItemID.IsNone())
+	{
+		const FItemData* ItemData = ItemDataTable->FindRow<FItemData>(DesiredItemID, DesiredItemID.ToString());
+		ItemReference = NewObject<UItemBase>(this, BaseClass);
+		ItemReference->ID = ItemData->ID;
+		ItemReference->TextData = ItemData->TextData;
+		ItemReference->ItemType = ItemData->ItemType;
+		ItemReference->ItemCondition = ItemData->ItemCondition;
+		ItemReference->ItemStatistics = ItemData->ItemStatistics;
+		ItemReference->NumericData = ItemData->NumericData;
+		ItemReference->AssetData = ItemData->AssetData;
+
+		InQuantity <= 0 ? ItemReference->SetQuantity(1) : ItemReference->SetQuantity(InQuantity);
+
+		PickupMesh->SetStaticMesh(ItemData->AssetData.Mesh);
+
+		UpdateInteractableData();
+	}
+}
+//~ Server Only
+
+void APickup::InitializeDrop(UItemBase* ItemToDrop, const int32 InQuantity)
+{
+	ItemReference = ItemToDrop;
+	InQuantity <= 0 ? ItemReference->SetQuantity(1) : ItemReference->SetQuantity(InQuantity);
+	ItemReference->NumericData.Weight = ItemToDrop->GetItemSingleWeight();
+	PickupMesh->SetStaticMesh(ItemToDrop->AssetData.Mesh);
+	UpdateInteractableData();
+}
+
+void APickup::Interact(APlayerCharacter* PlayerCharacter)
+{
+	// TODO: might need to be authoritative
+	if(PlayerCharacter)
+	{
+		TakePickup(PlayerCharacter);
+	}
+}
+
+void APickup::UpdateInteractableData()
+{
+	InstanceInteractableData.InteractableType = EInteractableType::EIT_Pickup;
+	InstanceInteractableData.Action = ItemReference->TextData.Interaction;
+	InstanceInteractableData.Name = ItemReference->TextData.Name;
+	InstanceInteractableData.Quantity = ItemReference->Quantity;
+	InteractableData = InstanceInteractableData;
+}
+
+void APickup::TakePickup(const APlayerCharacter* Taker)
+{
+	if (IsValid(this))
+	{
+		if(ItemReference)
+		{
+			// if(UInventoryComponent* PlayerInventory = Taker->GetInventory())
+			// {
+			// 	try to add item to player inventory, adjust or destroy item
+			// }
+		}
+	}
 }
 
 void APickup::Destroyed()
--- Source/Uchronia/Public/Item/Pickup.h
+++ Source/Uchronia/Public/Item/Pickup.h
@@ -3,42 +3,77 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "Character/PlayerCharacter.h"
 #include "GameFramework/Actor.h"
+#include "Interaction/InteractionInterface.h"
 #include "Pickup.generated.h"
 
+class UItemBase;
 class USphereComponent;
 
 UCLASS()
-class UCHRONIA_API APickup : public AActor
+class UCHRONIA_API APickup : public AActor, public IInteractionInterface
 {
 	GENERATED_BODY()
 	
 public:	
 	APickup();
-	virtual void Tick(float DeltaTime) override;
 	virtual void Destroyed() override;
+
+#if WITH_EDITOR
+	virtual void PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent) override;
+#endif
+
+	//~ Interaction Interface
+	virtual void BeginFocus() override;
+	virtual void EndFocus() override;
+	//~ Interaction Interface
+	
+	void InitializePickup(const TSubclassOf<UItemBase> BaseClass, const int32 InQuantity);
+	void InitializeDrop(UItemBase* ItemToDrop, const int32 InQuantity);
 
 protected:
 	virtual void BeginPlay() override;
 
 	UFUNCTION()
 	virtual void OnBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult);
-
-private:
-	UPROPERTY(EditAnywhere, Category="Pickup Properties")
-	TObjectPtr<USphereComponent> OverlapSphere;
 	
-	UPROPERTY(EditAnywhere, Category="Pickup Properties")
+	//~ Interaction Interface
+	virtual void Interact(APlayerCharacter* PlayerCharacter) override;
+	//~ Interaction Interface
+
+	void UpdateInteractableData();
+	void TakePickup(const APlayerCharacter* Taker);
+	
+	UPROPERTY(EditInstanceOnly, Category="Pickup Properties | Initialization")
+	FName DesiredItemID;
+	
+	UPROPERTY(EditInstanceOnly, Category="Pickup Properties | Initialization")
+	TObjectPtr<UDataTable> ItemDataTable;
+	
+	UPROPERTY(EditInstanceOnly, Category="Pickup Properties | Initialization")
+	int32 ItemQuantity;
+	
+	UPROPERTY(EditInstanceOnly, Category="Pickup Properties | Initialization")
 	TObjectPtr<UStaticMeshComponent> PickupMesh;
 
-	UPROPERTY(EditAnywhere, Category="Pickup Properties")
+	UPROPERTY(EditInstanceOnly, Category="Pickup Properties | Initialization")
 	TObjectPtr<USoundBase> PickupSound;
-	
-	UPROPERTY(EditAnywhere, Category="Pickup Properties")
-	float OverlapRadius = 100.f;
 
+	UPROPERTY(VisibleAnywhere, Category="Pickup Properties")
+	TObjectPtr<UItemBase> ItemReference;
+
+	UPROPERTY(VisibleInstanceOnly, Category="Pickup Properties")
+	FInteractableData InstanceInteractableData;
 	
+private:
+	UPROPERTY(EditDefaultsOnly, Category="Pickup Properties")
+	TObjectPtr<USphereComponent> OverlapSphere;
+	
+	UPROPERTY(EditDefaultsOnly, Category="Pickup Properties")
+	float OverlapRadius = 100.f;
 	
 public:	
-
+	FORCEINLINE UItemBase* GetItemData() const { return ItemReference; }
+	
 };
